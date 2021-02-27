045a4ce1cb79e55270f0c4859b165b55807527ce
kbrady-rgb <58741813+kbrady-rgb@users.noreply.github.com>
2021-02-26 22:31:44 -0600

    Initialization

---
+++ LinkedListStack/LinkedListStack/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedListStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
